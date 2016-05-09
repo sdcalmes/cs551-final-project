109adfe01b99c47492957ad2f6f03e1593d3a1f6
Frank Barry-Lenoch <barrylenoch@wisc.edu>
2016-05-09 09:01:01 -0500

    added some more inputs to forwarding

--- demo3/verilog/forwarding.v
+++ demo3/verilog/forwarding.v
@@ -1,4 +1,9 @@
 module forwarding(id_rs, id_rt, em_rd, mw_rd, forw_em, forw_mw)
+    input mem_regWrite, wb_regWrite;
     input [3:0] id_rs, id_rt, em_rd, mw_rd;
+	input [4:0] id_opcode, mem_opcode, wb_opcode;
+	//input [15:0] 
+	
+	output [15:0] reg1_forward, reg2_forward;
 
 endmodule
